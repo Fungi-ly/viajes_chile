b01643932420d26fe78b37a70c64529f21be3260
Eduardo León <106458374+Fungi-ly@users.noreply.github.com>
2022-06-03 19:26:36 -0400

    viajes_chile
    se carga viaje chile proyecto

---
+++ Viajes_Chile/assets/js/index.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function () {
+
+    $("a").click(function ()
+    
+    {var gato = this.hash
+    
+    $("html, body").animate(
+        {
+     
+      scrollTop: $(gato).offset().top -70
+     },
+     800
+    )
+    })
+    })
+    
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    
+    
+    
+   
+    
